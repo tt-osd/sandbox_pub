dd8f70bc2494e086788ce466d9bd2f80e39f1fdd
lev <levittshanhe@Florence.local>
2020-06-11 15:47:54 +0200

    chat tag and remove admin account info

--- assets/tealium.js
+++ assets/tealium.js
@@ -10,50 +10,30 @@ function tealium(){
       });
   }
   //load script function
-  
-   var the_url= window.location.href;
-   //using Support API to get user information
-   var the_domain_header=the_url.split(".com");
-   var the_url_header =the_domain_header[0];
-   var xhr = new XMLHttpRequest();
-   var url = the_url_header+".com/api/v2/users/me.json";
-   username = "ccdev@groups.tomtom.com",
-        password = "@=lx4+68}g";
-        xhr.withCredentials = true;
-        xhr.open("GET", url, true, username, password);
-   
-   xhr.send();      
-
-
-   xhr.addEventListener("readystatechange", function() {
-    if(this.readyState === 4) {
-
-    var user_tomtom_id = (JSON.parse(this.responseText)).user.user_fields.tomtom_id;
-   
+      
    //tealium
    //get utag script from MAP
    var id_map_data_source="https:"+$("#map_json").html();
    $.getJSON(id_map_data_source, function(data){
        var tealium_script="";
-      // console.log(data);
+    
 
       //loading the script
    if (the_url.indexOf("sandbox")!= -1) {
     tealium_script=data.tealium.sandbox;
    
-   // $.loadScript('//tags.tiqcdn.com/utag/tomtom/support/qa/utag.js');
+  
     }
     
     if (the_url.indexOf("sandbox")== -1) {
         tealium_script=data.tealium.prod;
-   // $.loadScript('//tags.tiqcdn.com/utag/tomtom/support/prod/utag.js');
+  
     }
     //loading the script
  
     $.loadScript(tealium_script);
 
-    //console.log("tealium_script  "+tealium_script);
-
+ 
    });
       
 
@@ -177,14 +157,14 @@ if (language.match(/^(en-ca|en-us|fr-ca|pt-br|es-ar|es-mx)$/)) {
     page_category: page_category,    
     page_type_id:page_type_id,
     page_id: page_id,
-    user_id:user_tomtom_id
+    user_id:0
   
   };
   
   
   console.log(utag_data);
    //tealium
-    }});
+
 
 
    
