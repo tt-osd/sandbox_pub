2568de5fdd3b7085dcba3e38bcaba29fd63f5207
lev <levittshanhe@Florence.local>
2020-10-27 16:41:09 +0100

    GDPR attachment remove after 30 days

--- assets/Attachtment_validation.js
+++ assets/Attachtment_validation.js
@@ -1,5 +1,7 @@
 var comment_list = document.getElementById("comment_list");
+
 if (comment_list != null) {
+
     var each_in_list = comment_list.getElementsByClassName("comment");
     if (each_in_list != null) {
 
@@ -19,15 +21,14 @@ if (comment_list != null) {
                         var today = today_raw.toJSON()
 
 
-                        console.log("time_num " + time_num);
-                        console.log("today " + today);
+
 
                         var init = new Date(time_num);
                         var rightNow = new Date(today);
                         var dif = rightNow.getTime() - init.getTime();
                         var dif_in_days = dif / (1000 * 3600 * 24)
 
-                        console.log("DIFF in days" + dif_in_days)
+
                         if (dif_in_days > 30) {
                             this_attachment[x].classList.add("zd_Hidden");
                         }
